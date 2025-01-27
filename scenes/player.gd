f4ec318730397a71d3a9205e166d730ee84a8799
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-01-27 10:54:31 -0500

    Update With Existing Project

---
+++ scenes/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody3D
+
+@export var turn_rate := 5.0
+@export var directional_reference: Node3D
+@export var player_model: Node3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (directional_reference.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	# Rotate player model to look in movement direction
+	if direction != Vector3.ZERO:
+		var look_dir = atan2(-velocity.x, -velocity.z)
+		player_model.rotation.y = lerp_angle(player_model.rotation.y, look_dir, delta * turn_rate)
+	
+	move_and_slide()
