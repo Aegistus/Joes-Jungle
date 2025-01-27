2560dc6b3415eb6c1db0ea48d05cbf9594856412
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-01-27 18:42:04 -0500

    Add Walk and Idle Animations

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 @export var turn_rate := 5.0
 @export var directional_reference: Node3D
 @export var player_model: Node3D
+@export var animation_player: AnimationPlayer
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -23,16 +24,19 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (directional_reference.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	# Rotate player model to look in movement direction
 	if direction != Vector3.ZERO:
 		var look_dir = atan2(-velocity.x, -velocity.z)
 		player_model.rotation.y = lerp_angle(player_model.rotation.y, look_dir, delta * turn_rate)
+	
+	if direction != Vector3.ZERO:
+		animation_player.play("walking")
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		animation_player.play("idle")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	move_and_slide()
