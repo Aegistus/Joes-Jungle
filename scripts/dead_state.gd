3946c1e06f4a61da91b373931650d4438a7aa8bd
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-01-31 15:08:21 -0500

    Remove Dead Zombies After Delay

--- scripts/dead_state.gd
+++ scripts/dead_state.gd
@@ -1,6 +1,8 @@
 class_name DeadState
 extends ZombieState
 
+@export var despawn_delay = 5.0
+
 @onready var ragdoll = $"../../zombie_model/Armature/GeneralSkeleton/PhysicalBoneSimulator3D"
 @onready var hip_bone = $"../../zombie_model/Armature/GeneralSkeleton/PhysicalBoneSimulator3D/Physical Bone Hips"
 @onready var collision_shape_3d = $"../../CollisionShape3D"
@@ -11,3 +13,9 @@ func enter():
 	hip_bone.linear_velocity = Vector3.BACK * 10
 	ragdoll.active = true
 	ragdoll.physical_bones_start_simulation()
+	var timer = Timer.new()
+	self.add_child(timer)
+	timer.timeout.connect(zombie.queue_free)
+	timer.wait_time = despawn_delay
+	timer.start()
+	
