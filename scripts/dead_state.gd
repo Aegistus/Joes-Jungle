2c4990da85ca4c047a39e88ba040c7714bda8c61
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-01-31 14:25:48 -0500

    Add Better Ragdoll's with Jolt Physics

--- scripts/dead_state.gd
+++ scripts/dead_state.gd
@@ -2,8 +2,12 @@ class_name DeadState
 extends ZombieState
 
 @onready var ragdoll = $"../../zombie_model/Armature/GeneralSkeleton/PhysicalBoneSimulator3D"
+@onready var hip_bone = $"../../zombie_model/Armature/GeneralSkeleton/PhysicalBoneSimulator3D/Physical Bone Hips"
+@onready var collision_shape_3d = $"../../CollisionShape3D"
 
 func enter():
+	collision_shape_3d.disabled = true
 	zombie.velocity = Vector3.ZERO
+	hip_bone.linear_velocity = Vector3.BACK * 10
 	ragdoll.active = true
 	ragdoll.physical_bones_start_simulation()
