cf9335be3dc88761a738aba9a29191144592de66
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-01-30 19:54:19 -0500

    Adjust Player Move Speeds

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,38 +6,18 @@ extends CharacterBody3D
 @export var player_model: Node3D
 @export var animation_player: AnimationPlayer
 
+@onready var pistol = $"PlayerModel/Model/Armature/GeneralSkeleton/BoneAttachment3D/PistolHolder/Pistol 92"
+@onready var pistol_anim = pistol.get_node("AnimationPlayer") as AnimationPlayer
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	#var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	#var direction = (directional_reference.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-	# Rotate player model to look in movement direction
-	#if direction != Vector3.ZERO:
-		#var look_dir = atan2(-velocity.x, -velocity.z)
-		#player_model.rotation.y = lerp_angle(player_model.rotation.y, look_dir, delta * turn_rate)
-	
-	#if direction != Vector3.ZERO:
-		#animation_player.play("walking")
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#animation_player.play("idle")
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-	#
-	move_and_slide()
+	if Input.is_action_just_pressed("shoot"):
+		pistol_anim.play("shoot")
