4e35e0a8c48f3ab641ddfa3808bfb82263447c0b
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-01-31 14:58:02 -0500

    Add Zombie Spawner

---
+++ scripts/zombie_spawn_point.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export var initial_delay = 3.0
+@export var spawn_delay = 8.0
+@export var delay_decrease = 0.1
+
+@onready var timer = $Timer
+
+var current_delay
+const ZOMBIE_SCENE = preload("res://scenes/zombie.tscn")
+
+func _ready():
+	current_delay = spawn_delay
+	timer.wait_time = initial_delay
+	timer.start()
+	
+
+func _on_timer_timeout():
+	var zombie = ZOMBIE_SCENE.instantiate()
+	get_parent_node_3d().add_child(zombie)
+	zombie.position = position
+	current_delay = clampf(current_delay - delay_decrease, 1, 1000)
+	timer.wait_time = current_delay
+	timer.start()
