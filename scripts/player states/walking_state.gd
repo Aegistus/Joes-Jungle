3562e30696367b58a1c9cc6af2b900af978b1dc3
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-01-31 19:26:00 -0500

    Fix Being Able to Shoot with No Ammo

--- scripts/player states/walking_state.gd
+++ scripts/player states/walking_state.gd
@@ -29,13 +29,15 @@ func process_state_physics(delta):
 	var relative_dir = (player_model.basis * Vector3(direction.z, 0, direction.x)).normalized()
 	animation_tree.set("parameters/WalkSpeed/blend_position", Vector2(relative_dir.z, -relative_dir.x))
 	# handle shooting
-	if Input.is_action_just_pressed("shoot"):
-		var collided = controlled_player.raycast.get_collider()
-		if collided != null:
-			print(collided.name)
-		if collided != null and collided.is_in_group("enemy"):
-			collided.hit(randi_range(controlled_player.min_damage, controlled_player.max_damage))
-		controlled_player.pistol_92.shoot()
+	if aim_state_machine.current_state is AimingState and Input.is_action_just_pressed("shoot"):
+		var success = controlled_player.pistol_92.shoot()
+		if success:
+			var collided = controlled_player.raycast.get_collider()
+			if collided != null:
+				print(collided.name)
+			if collided != null and collided.is_in_group("enemy"):
+				collided.hit(randi_range(controlled_player.min_damage, controlled_player.max_damage))
+
 
 func check_transitions():
 	if Input.is_action_just_pressed("reload"):
