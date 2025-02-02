847b597c3fb212f6de178beb57f285196ef6fbb2
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-01 19:02:49 -0500

    Add Final Time Score to Game Over Scene

--- scripts/game_over_scene.gd
+++ scripts/game_over_scene.gd
@@ -1,6 +1,16 @@
 extends Node3D
 
+@onready var final_time = $UI/CanvasLayer/Panel/FinalTime
+
+func _ready():
+	var minutes : int = GameManager.run_time / 60
+	var seconds := fmod(GameManager.run_time, 60)
+	var milliseconds := fmod(GameManager.run_time, 1) * 100
+	var time_string := "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
+	final_time.text = "Final Time: " + time_string
+
 func _on_restart_button_pressed():
+	GameManager.is_game_running = true
 	get_tree().change_scene_to_file("res://scenes/build_scenes/main.tscn")
 
 func _on_main_menu_button_pressed():
