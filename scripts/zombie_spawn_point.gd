8d301cd8ea7c308b7d59afba132f78a2a73ec747
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-03 13:30:37 -0500

    Rework Zombie Spawns to Use Waves Rather than Continuous Spawning

--- scripts/zombie_spawn_point.gd
+++ scripts/zombie_spawn_point.gd
@@ -1,29 +1,39 @@
 extends Node3D
 
 @export var initial_delay = 3.0
-@export var spawn_delay = 15
-@export var delay_decrease = 1
-@export var min_spawn_delay = 5
+@export var wave_delay = 30.0
 
-@onready var timer = $Timer
+@onready var wave_timer = $WaveTimer
+@onready var spawn_timer = $SpawnTimer
+@onready var spawn_point_parent = $SpawnPointParent
+
+var current_wave_spawn_count = 5
+var all_spawn_points
+var spawn_point_count
 
 const MAX_ZOMBIES = 100
-
-var current_delay
 const ZOMBIE_SCENE = preload("res://scenes/zombie.tscn")
 
 func _ready():
-	current_delay = spawn_delay
-	timer.wait_time = initial_delay
-	timer.start()
-	
+	wave_timer.wait_time = initial_delay
+	wave_timer.start()
+	all_spawn_points = spawn_point_parent.get_children() as Array[Node3D]
+	spawn_point_count = all_spawn_points.size()
 
 func _on_timer_timeout():
+	spawn_wave()
+
+func spawn_wave():
 	var all_zombies = get_tree().get_nodes_in_group("enemy")
-	if all_zombies.size() < MAX_ZOMBIES:
-		var zombie = ZOMBIE_SCENE.instantiate()
-		get_parent_node_3d().add_child(zombie)
-		zombie.position = position
-		current_delay = clampf(current_delay - delay_decrease, 1, 1000)
-	timer.wait_time = current_delay
-	timer.start()
+	for i in range(0, current_wave_spawn_count):
+		all_zombies = get_tree().get_nodes_in_group("enemy")
+		if all_zombies.size() < MAX_ZOMBIES:
+			var zombie = ZOMBIE_SCENE.instantiate()
+			get_parent_node_3d().add_child(zombie)
+			zombie.position = all_spawn_points[i % spawn_point_count].position
+		spawn_timer.wait_time = .1
+		spawn_timer.start()
+		await spawn_timer.timeout
+	current_wave_spawn_count += 1
+	wave_timer.wait_time = wave_delay
+	wave_timer.start()
