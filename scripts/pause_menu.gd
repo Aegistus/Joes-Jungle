5bd269d3ec30e2f846a4b367dfd0d0ea17533d09
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-06 19:21:01 -0500

    Add Pause Menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("pause"):
+		toggle_pause()
+
+func toggle_pause():
+	if get_tree().paused == true:
+		hide()
+		get_tree().paused = false
+		Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+	elif get_tree().paused == false:
+		show()
+		get_tree().paused = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func exit_to_main_menu():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/game_scenes/main_menu.tscn")
+
+func exit_game():
+	get_tree().paused = false
+	get_tree().quit()
