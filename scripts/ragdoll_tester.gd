8a01cf3e84d0d14576b28ffe85b6238a6934e003
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-08 14:36:55 -0500

    Adjust Zombie Ragdoll Leg Movements

--- scripts/ragdoll_tester.gd
+++ scripts/ragdoll_tester.gd
@@ -1,6 +1,26 @@
 extends Node
 
+@export var spawn_interval = 5
+
+const ZOMBIE = preload("res://scenes/enemies/zombie.tscn")
+const TANK_ZOMBIE = preload("res://scenes/enemies/tank_zombie.tscn")
+const FAST_ZOMBIE = preload("res://scenes/enemies/fast_zombie.tscn")
+
+var timer
+
 func _ready():
-	var enemies = get_tree().get_nodes_in_group("enemy")
-	for i in enemies.size():
-		enemies[i].hit(1000, Vector3.FORWARD, Vector3.BACK)
+	timer = Timer.new()
+	add_child(timer, false, Node.INTERNAL_MODE_FRONT)
+	timer.wait_time = spawn_interval
+	timer.timeout.connect(spawn_new)
+	spawn_new()
+
+
+func spawn_new():
+	for i in get_child_count():
+		var zombie = ZOMBIE.instantiate()
+		get_child(i).add_child(zombie)
+		zombie.position = Vector3.ZERO
+		zombie.rotate(Vector3.UP, 180)
+		zombie.hit(1000, Vector3.FORWARD, Vector3.FORWARD)
+	timer.start()
