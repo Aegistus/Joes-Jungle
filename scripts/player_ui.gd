cd9af53c4efa3c26a29b1303d5843dc0efcb836d
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-09 15:10:40 -0500

    Update Health Bar UI to Look Nice 😃

--- scripts/player_ui.gd
+++ scripts/player_ui.gd
@@ -2,13 +2,14 @@ extends Control
 
 @onready var interactable_tooltip = $CanvasLayer/InteractableTooltip
 @onready var tooltip_label = $CanvasLayer/InteractableTooltip/Label
-@onready var health_bar = $CanvasLayer/HealthBar
+@onready var health_bar = $CanvasLayer/DamageBar/HealthBar
+@onready var damage_bar = $CanvasLayer/DamageBar
 
 var player
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player") as Player
-	player.health_update.connect(func(health, health_max): health_bar.value = health / health_max * 100)
+	player.health_update.connect(update_health)
 	health_bar.value = 100
 
 func _process(delta):
@@ -17,3 +18,15 @@ func _process(delta):
 		tooltip_label.text = player.current_interactable.tooltip_text
 	else:
 		interactable_tooltip.visible = false
+
+func update_health(new_health, health_max):
+	if float(new_health) / health_max > health_bar.value: # if healing
+		var tween = create_tween()
+		tween.tween_property(health_bar, "value", float(new_health) / health_max, 1)
+		var tween2 = create_tween()
+		tween2.tween_property(damage_bar, "value", float(new_health) / health_max, 1)
+	else: # if taking damage
+		health_bar.value = float(new_health) / health_max
+		await get_tree().create_timer(.5).timeout
+		var tween = create_tween()
+		tween.tween_property(damage_bar, "value", float(new_health) / health_max, 1)
