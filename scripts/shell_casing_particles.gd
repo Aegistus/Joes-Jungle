18576a050b60ac590e1af6897fd9178612bdf9c3
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-09 20:53:30 -0500

    Add Shell Casing SFX

--- scripts/shell_casing_particles.gd
+++ scripts/shell_casing_particles.gd
@@ -1,6 +1,10 @@
 extends GPUParticles3D
 
+@onready var audio_stream_player_3d = $AudioStreamPlayer3D
 
 func _process(delta):
 	if not emitting:
 		queue_free()
+
+func _on_timer_timeout():
+	audio_stream_player_3d.play()
