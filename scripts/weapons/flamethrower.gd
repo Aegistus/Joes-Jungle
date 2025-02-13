a16f864a67ad7fe902a1d2644e51eeacadc47d71
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-12 21:59:59 -0500

    Make Flamethrower Light Fade In/Out Gradually

--- scripts/weapons/flamethrower.gd
+++ scripts/weapons/flamethrower.gd
@@ -1,21 +1,26 @@
 class_name Flamethrower
 extends Gun
 
+@export var max_light_energy := 6.0
+@export var light_up_time := .5
+@export var dim_light_time := 1.5
+@export var light_color : Color
+
 @onready var fire_particles = $gun_model/Fire_Particles
 @onready var flame_hitbox : ShapeCast3D = $gun_model/FlameHitbox
 @onready var flame_check_timer = $FlameCheckTimer
 @onready var flamethrower_start_audio_player = $FlamethrowerStartAudioPlayer
 @onready var flamethrower_loop_audio_player = $FlamethrowerLoopAudioPlayer
-@onready var omni_light_3d = $gun_model/OmniLight3D
+@onready var light_spawn_point = $gun_model/LightSpawnPoint
 
 const FIRE = preload("res://scenes/particles/fire.tscn")
 const FLAMETHROWER_TANK = preload("res://scenes/weapons/flamethrower_tank.tscn")
 
 var backpack
+var light
 
 func _ready():
 	flame_hitbox.enabled = false
-	omni_light_3d.visible = false
 	flame_check_timer.timeout.connect(set_targets_on_fire)
 
 func shoot():
@@ -24,14 +29,22 @@ func shoot():
 	flame_check_timer.start()
 	flamethrower_start_audio_player.play()
 	flamethrower_loop_audio_player.play()
-	omni_light_3d.visible = true
+	light = OmniLight3D.new()
+	get_parent_node_3d().add_child(light)
+	light.global_position = light_spawn_point.global_position
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "light_energy", max_light_energy, light_up_time)
 
 func shoot_end():
 	fire_particles.stop()
 	flame_hitbox.enabled = false
 	flame_check_timer.stop()
 	flamethrower_loop_audio_player.stop()
-	omni_light_3d.visible = false
+	if light != null:
+		light.reparent(get_tree().root)
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "light_energy", 0, dim_light_time)
+	tween.tween_callback(light.queue_free).set_delay(dim_light_time)
 
 func set_targets_on_fire():
 	var hits = flame_hitbox.collision_result as Array[Node]
