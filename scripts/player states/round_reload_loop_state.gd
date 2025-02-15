e53b18efe77e9bd98b37776b6cb64197115e75c3
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-15 00:24:27 -0500

    Add Moving While Rounds Reloading
    Holy shit I did it!!!!

--- scripts/player states/round_reload_loop_state.gd
+++ scripts/player states/round_reload_loop_state.gd
@@ -7,6 +7,7 @@ extends PlayerState
 @onready var round_reload_end_state = %RoundReloadEndState
 @onready var idle_state = %IdleState
 @onready var directional_reference = $"../../CameraMount/Camera3D/Directional_Reference"
+@onready var round_reload_anim_tree = $"../../PlayerModel/Model/RoundReloadAnimTree" as AnimationTree
 
 var finished = false
 var cancel = false
@@ -16,8 +17,14 @@ func enter():
 	controlled_player.current_animation_tree.active = false
 	finished = false
 	cancel = false
-	animation_player.play("Michael Misc/single round reload loop")
-	animation_player.animation_finished.connect(func(_name): finished = true)
+	round_reload_anim_tree.active = true
+	#animation_player.play("Michael Misc/single round reload loop")
+	round_reload_anim_tree.animation_finished.connect(
+		func(_name): 
+			if _name == "Michael Misc/single round reload loop":
+				finished = true
+	)
+	%AimStateMachine.transition_to(%RelaxedState)
 
 func process_state_physics(delta):
 	if Input.is_action_just_pressed("shoot"):
@@ -25,10 +32,10 @@ func process_state_physics(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (directional_reference.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction != Vector3.ZERO:
-		target.velocity.x = direction.x * walk_speed * delta
-		target.velocity.z = direction.z * walk_speed * delta
+	target.velocity.x = direction.x * walk_speed * delta
+	target.velocity.z = direction.z * walk_speed * delta
 	target.move_and_slide()
+	round_reload_anim_tree.set("parameters/Movement/blend_position", direction.length())
 
 func check_transitions() -> State:
 	if finished:
@@ -36,11 +43,11 @@ func check_transitions() -> State:
 		if controlled_player.gun.ammo.can_reload():
 			return self
 		else:
-			return round_reload_end_state
+			return idle_state
 	if cancel:
 		return idle_state
 	return null
 
 func exit():
 	controlled_player.current_animation_tree.active = true
-	
+	round_reload_anim_tree.active = false
