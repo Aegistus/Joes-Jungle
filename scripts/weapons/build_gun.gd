ea15047e6cb4309a2557b91391eca001ed19fec3
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-17 14:49:25 -0500

    Add Claymore Emplacement and Allow Switching to Different Emplacement with Build Gun

--- scripts/weapons/build_gun.gd
+++ scripts/weapons/build_gun.gd
@@ -2,19 +2,21 @@ class_name BuildGun
 extends Gun
 
 const BARRICADE_EMPLACEMENT = preload("res://scenes/emplacements/barricade_emplacement.tscn")
+const CLAYMORE_MINE = preload("res://scenes/emplacements/claymore_mine.tscn")
 
-var all_builds = [BARRICADE_EMPLACEMENT, ]
+var all_builds = [BARRICADE_EMPLACEMENT, CLAYMORE_MINE]
 var currently_selected_build
+var current_index = 0
 var current_emplacement_ghost : Node3D
 var is_equipped = false
 var player : Player
 var reticle
 
 func _ready():
-	currently_selected_build = all_builds[0]
+	currently_selected_build = all_builds[current_index]
 
 func shoot():
-	var emplacement = BARRICADE_EMPLACEMENT.instantiate() as Emplacement
+	var emplacement = currently_selected_build.instantiate() as Emplacement
 	get_tree().root.add_child(emplacement)
 	emplacement.global_position = reticle.global_position
 	emplacement.global_rotation = current_emplacement_ghost.global_rotation
@@ -38,6 +40,12 @@ func _physics_process(delta):
 		elif current_emplacement_ghost != null:
 			current_emplacement_ghost.visible = true
 			current_emplacement_ghost.global_position = reticle.global_position
+		if Input.is_action_just_pressed("emplacements_scroll_down"):
+			current_index = (current_index - 1) % all_builds.size()
+			select_new_emplacement()
+		if Input.is_action_just_pressed("emplacements_scroll_up"):
+			current_index = (current_index + 1) % all_builds.size()
+			select_new_emplacement()
 
 func spawn_emplacement_ghost():
 	current_emplacement_ghost = currently_selected_build.instantiate() as Emplacement
@@ -50,3 +58,8 @@ func rotate_right():
 
 func rotate_left():
 	current_emplacement_ghost.rotate_object_local(Vector3.UP, -PI/4.0)
+
+func select_new_emplacement():
+	currently_selected_build = all_builds[current_index]
+	current_emplacement_ghost.queue_free()
+	spawn_emplacement_ghost()
