4ade37a885e5c6e2f74967a8e8966fc7ea188955
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-19 16:52:41 -0500

    Make Plant Water Icon Turn Red When Below Half Water

--- scripts/ui/water_bars_ui.gd
+++ scripts/ui/water_bars_ui.gd
@@ -1,22 +1,37 @@
 extends VBoxContainer
 
-@onready var plant_a_prog = $HBoxContainer/ProgressBar
-@onready var plant_b_prog = $HBoxContainer2/ProgressBar
-@onready var plant_c_prog = $HBoxContainer3/ProgressBar
+@export var default_color : Color
+@export var warning_color : Color
+@export var plant_progress_bars : Array[ProgressBar]
 
-var plant_a : WateringBush
-var plant_b : WateringBush
-var plant_c : WateringBush
+var watering_bushes : Array[WateringBush] = [null, null, null]
+
+var plant_low : Array[bool] = [false,false,false]
+
+var defaultStyleBox : StyleBoxFlat
+var warningStyleBox : StyleBoxFlat
 
 func _ready():
+	defaultStyleBox = StyleBoxFlat.new()
+	defaultStyleBox.bg_color = default_color
+	warningStyleBox = StyleBoxFlat.new()
+	warningStyleBox.bg_color = warning_color
 	var all_plants = get_tree().get_nodes_in_group("plant")
 	for i in all_plants.size():
 		match (all_plants[i].id):
-			0: plant_a = all_plants[i]
-			1: plant_b = all_plants[i]
-			2: plant_c = all_plants[i]
+			0: watering_bushes[0] = all_plants[i] as WateringBush
+			1: watering_bushes[1] = all_plants[i] as WateringBush
+			2: watering_bushes[2] = all_plants[i] as WateringBush
 
 func _process(delta):
-	plant_a_prog.value = plant_a.current_water / plant_a.max_water * 100
-	plant_b_prog.value = plant_b.current_water / plant_b.max_water * 100
-	plant_c_prog.value = plant_c.current_water / plant_c.max_water * 100
+	for i in watering_bushes.size():
+		update_bush_ui(i)
+
+func update_bush_ui(i : int):
+	plant_progress_bars[i].value = watering_bushes[i].current_water / watering_bushes[i].max_water * 100
+	if watering_bushes[i].current_water <= watering_bushes[i].max_water / 2.0 and !plant_low[i]:
+		plant_low[i] = true
+		plant_progress_bars[i].add_theme_stylebox_override("fill", warningStyleBox)
+	if watering_bushes[i].current_water > watering_bushes[i].max_water / 2.0 and plant_low[i]:
+		plant_low[i] = false
+		plant_progress_bars[i].add_theme_stylebox_override("fill", defaultStyleBox)
