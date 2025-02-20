cae768bbd3df1298edd7c86291a59f61b897020f
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-20 14:28:41 -0500

    Decrease Sell Time During Intermission

--- scripts/emplacements/emplacement_seller.gd
+++ scripts/emplacements/emplacement_seller.gd
@@ -3,12 +3,19 @@ extends Interactable
 
 signal on_sell
 
+@export var wave_sell_delay = 5
+@export var intermission_sell_delay = 1
+
 @onready var timer = $Timer
 
 func _ready():
 	timer.timeout.connect(func(): on_sell.emit())
 
 func interact_start():
+	if GameManager.currently_in_wave:
+		timer.wait_time = wave_sell_delay
+	else:
+		timer.wait_time = intermission_sell_delay
 	timer.start()
 
 func interact_end():
