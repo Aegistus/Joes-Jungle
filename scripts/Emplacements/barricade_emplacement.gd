c138520d5643736fb4b00e67403e2e245df84bc5
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-02-26 20:12:33 -0500

    Add Slightly Damaged and Badly Damaged Barricade Models

--- scripts/Emplacements/barricade_emplacement.gd
+++ scripts/Emplacements/barricade_emplacement.gd
@@ -8,8 +8,10 @@ signal on_destroy
 @onready var actor_collision_shape = $ActorCollision/ActorCollisionShape
 @onready var hurtbox = $Hurtbox
 @onready var animation_player = %AnimationPlayer
-@onready var model = %Model
 @onready var emplacement_seller = $EmplacementSeller
+@onready var healthy_model = %HealthyModel
+@onready var slightly_damaged_model = $SlightlyDamagedModel
+@onready var badly_damaged_model = $BadlyDamagedModel
 
 var current_health
 
@@ -23,12 +25,23 @@ func place():
 	current_health = max_health
 	hurtbox.monitoring = true
 	hurtbox.monitorable = true
-	model.visible = true
+	# models
+	healthy_model.visible = true
+	slightly_damaged_model.visible = false
+	badly_damaged_model.visible = false
 	actor_collision_shape.disabled = false
 	GameManager.on_barricade_added.emit(self)
 
 func damage(amount):
 	current_health -= amount
+	if current_health < max_health / 2 and current_health > max_health / 4:
+		healthy_model.visible = false
+		slightly_damaged_model.visible = true
+		badly_damaged_model.visible = false
+	if current_health < max_health / 4:
+		healthy_model.visible = false
+		slightly_damaged_model.visible = false
+		badly_damaged_model.visible = true
 	if current_health <= 0:
 		destroy()
 
@@ -36,7 +49,9 @@ func destroy():
 	hurtbox.monitoring = false
 	hurtbox.set_deferred("monitorable", false)
 	actor_collision_shape.disabled = true
-	model.visible = false
+	healthy_model.visible = false
+	slightly_damaged_model.visible = false
+	badly_damaged_model.visible = false
 	on_destroy.emit()
 	GameManager.on_barricade_destroyed.emit()
 
