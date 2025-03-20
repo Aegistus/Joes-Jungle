88af1263d31a824ad6aefae8ed5d6984f40d2fb2
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-03-20 16:51:59 -0400

    Add Jungle Ambiance That Quiets When Indoors

---
+++ scripts/ambient_sfx.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@onready var jungle_sounds = $JungleSounds
+
+var default_volume
+var volume_decrease = -20
+var transition_time = 1
+
+func _ready():
+	default_volume = jungle_sounds.volume_db
+
+func on_player_enter(area):
+	var tween = get_tree().create_tween()
+	tween.tween_property(jungle_sounds, "volume_db", default_volume - 10, transition_time)
+	#jungle_sounds.volume_db = default_volume - 10
+
+func on_player_exit(area):
+	var tween = get_tree().create_tween()
+	tween.tween_property(jungle_sounds, "volume_db", default_volume, transition_time)
+	#jungle_sounds.volume_db = default_volume
