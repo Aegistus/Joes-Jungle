cd7ae15ead1cd74f67123360813527de49334164
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-03-20 14:17:23 -0400

    Add Footstep Sounds to Pistol Anims

---
+++ scripts/footsteps.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+@onready var wood_footstep_player = $WoodFootstepPlayer
+@onready var stone_footstep_player = $StoneFootstepPlayer
+@onready var grass_footstep_player = $GrassFootstepPlayer
+@onready var ray_cast_3d = $RayCast3D
+
+func play_footsteps():
+	var collider = ray_cast_3d.get_collider()
+	if collider is CSGShape3D:
+		var shape = collider as CSGShape3D
+		if shape.get_collision_layer_value(18): # wood
+			wood_footstep_player.play()
+		elif shape.get_collision_layer_value(19): # stone
+			stone_footstep_player.play()
+		elif shape.get_collision_layer_value(20): # grass
+			grass_footstep_player.play()
+	else:
+		wood_footstep_player.play()
