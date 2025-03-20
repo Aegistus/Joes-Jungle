204e24a7b30d36a3ea5df9ef9c9aa7c6ecf6c7e2
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-03-20 17:19:54 -0400

    Increase Number of Random Bones that Can Be Resized

--- scripts/insanity/random_bone_resizer.gd
+++ scripts/insanity/random_bone_resizer.gd
@@ -2,11 +2,15 @@ extends Skeleton3D
 
 @export var allowed_bones : Array[String] = ["Head", "Chest", "LeftUpperArm", "RightUpperArm", "LeftUpperLeg", "RightUpperLeg"]
 
-var min_scale_change = -.75
-var max_scale_change = .75
+var min_scale_change = -.5
+var max_scale_change = .5
+var min_bones_changed = 0
+var max_bones_changed = 10
 
 func _ready():
-	var bone = find_bone(allowed_bones.pick_random())
-	var random_scale = randf_range(min_scale_change, max_scale_change)
-	random_scale *= GameManager.current_insanity
-	set_bone_pose_scale(bone, Vector3(1 + random_scale, 1 + random_scale, 1 + random_scale))
+	var num_of_bones_changed = randi_range(min_bones_changed, max_bones_changed)
+	for i in num_of_bones_changed:
+		var bone = find_bone(allowed_bones.pick_random())
+		var random_scale = randf_range(min_scale_change, max_scale_change)
+		random_scale *= GameManager.current_insanity
+		set_bone_pose_scale(bone, Vector3(1 + random_scale, 1 + random_scale, 1 + random_scale))
