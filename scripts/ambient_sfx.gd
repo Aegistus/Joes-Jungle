5bf5d207d1f41eab15a4a6e508daf54646d840d3
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2025-03-20 17:07:27 -0400

    Add Horror Ambiance

--- scripts/ambient_sfx.gd
+++ scripts/ambient_sfx.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var jungle_sounds = $JungleSounds
+@onready var horror_ambiance = $HorrorAmbiance
 
 var default_volume
 var volume_decrease = -20
@@ -9,12 +10,13 @@ var transition_time = 1
 func _ready():
 	default_volume = jungle_sounds.volume_db
 
+func _process(delta):
+	horror_ambiance.volume_db = (1 - GameManager.current_insanity) * -80
+
 func on_player_enter(area):
 	var tween = get_tree().create_tween()
 	tween.tween_property(jungle_sounds, "volume_db", default_volume - 10, transition_time)
-	#jungle_sounds.volume_db = default_volume - 10
 
 func on_player_exit(area):
 	var tween = get_tree().create_tween()
 	tween.tween_property(jungle_sounds, "volume_db", default_volume, transition_time)
-	#jungle_sounds.volume_db = default_volume
